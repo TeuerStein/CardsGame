a8d4f7ba330221c5b15b5e4fc76f82da94f93b2e
TeuerStein <xenkai123456789@gmail.com>
2020-09-28 18:51:58 +0200

    Was added view a random emoji on objects

--- CardsGame/ViewController.swift
+++ CardsGame/ViewController.swift
@@ -48,8 +48,15 @@ class ViewController: UIViewController {
     
     var emojiChoices: Array<String> = ["🗿", "🤔", "🧐"]
     
+    var emoji = [Int:String]()
+    
     func emoji(for card: Card) -> String {
-        return "?"
+        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+        }
+        
+        return emoji[card.identifier] ?? "?"
     }
     
 }
