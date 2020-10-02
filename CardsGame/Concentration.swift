73fc6544bb114c9240d9034da9aa119307a4934a
TeuerStein <xenkai123456789@gmail.com>
2020-10-02 10:45:16 +0200

    Added a Collection extension for the private variable indexOfOneAndOnlyFaceUpCard

--- CardsGame/Concentration.swift
+++ CardsGame/Concentration.swift
@@ -12,17 +12,7 @@ struct Concentration{
     
     private var indexOfOneAndOnlyFaceUpCard: Int? {
         get {
-            var foundIndex: Int?
-            for index in cards.indices {
-                if cards[index].isFaceUp {
-                    if foundIndex == nil {
-                        foundIndex = index
-                    } else {
-                        return nil
-                    }
-                }
-            }
-            return foundIndex
+            return cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly
         }
         set {
             for index in cards.indices {
@@ -52,5 +42,11 @@ struct Concentration{
             let card = Card()
             cards += [card, card]
         }
+    }
+}
+
+extension Collection {
+    var oneAndOnly: Element? {
+        return count == 1 ? first : nil
     }
 }
